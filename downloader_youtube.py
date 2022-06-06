461ba79218b90a1965b66aa165cc767f8335e1db
forteeight6 <82338428+forteeight6@users.noreply.github.com>
2022-06-06 17:43:34 -0300

    Add files via upload

--- downloader_youtube.py
+++ downloader_youtube.py
@@ -1,3 +1,4 @@
+from pytube import YouTube, streams
 import re
 # !pip install pytube
 """
@@ -12,8 +13,6 @@ import re
     entered in the parameter.
 """
 
-from pytube import YouTube, streams
-
 def downloadAudioFromYoutube(*args) -> None:
     """ Download de Audio do Youtube / Youtube Audio download.
     
@@ -21,28 +20,37 @@ def downloadAudioFromYoutube(*args) -> None:
     Function that allows you to download audio from Youtube.
     
     :param - args[0]: Url of the audio you want to download.
-    :param - args[1]: Location where you want to save the audio. 
+    :param - args[1]: Location where you want to save the audio.
+    :return - None
     """
     
     youtube = YouTube(args[0])
+    
+    if (title is False) | (len(title) == 0):
+        title = re.sub(r"[/:,.?]","-", youtube.title)
+    
     title = youtube.title + ".mp3"
     stream = youtube.streams.get_audio_only()
-    stream.download(output_path=args[1], filename=title)
+    stream.download(output_path=args[1], filename=title + ".mp3")
     print('Download completed successfully!')
     return None
 
-def downloadVideoFromYoutube(*args) -> None:
+def downloadVideoFromYoutube(title=None, *args) -> None:
     """ Download de video do Youtube / Youtube video download.
     
     Função que possibilita o download de video do Youtube.
     Function that allows you to download video from Youtube.
     
+    :param - title: Denined title to save the video.
     :param - args[0]: Url of the video you want to download.
     :param - args[1]: Location where you want to save the video.
+    :return - None
     """
 
     youtube = YouTube(args[0])
-    title = re.sub(r"[/:,.´]"," ", youtube.title)
+    if (title is False) | (len(title) == 0):
+        title = re.sub(r"[/:,.?]","-", youtube.title)
+        
     print('Estou tentando baixar o seguinte video:\n', title)
     stream = youtube.streams.get_highest_resolution()
     
@@ -52,6 +60,6 @@ def downloadVideoFromYoutube(*args) -> None:
 
 if __name__ == "__main__":
     
-    url = "https://www.youtube.com/watch?v=0Ia_hnQoGmg"
+    url = "https://www.youtube.com/watch?v=jq6C3aIDkKI"
     location = "./"
-    downloadVideoFromYoutube(url, location)
+    downloadVideoFromYoutube("teste",url, location)
